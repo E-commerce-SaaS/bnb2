617aa7caad0cae00dc06b4f8795584a984eb6a37
sir-njuguna <sir.njuguna@outlook.com>
2026-08-05 15:43:32 +0300

    - Renamed endpoint for room update. - Introduced a filter to restrict users to only view rooms in their branch.

--- src/main/java/io/room/service/RoomReadService.java
+++ src/main/java/io/room/service/RoomReadService.java
@@ -1,10 +1,12 @@
 package io.room.service;
 
+import io.internaluser.service.InternalUserReadService;
 import io.micrometer.common.util.StringUtils;
 import io.room.entity.Room;
 import io.room.repository.RoomRepository;
 import io.lib.service.BaseJpaRepoReadService;
 import io.room.form.FetchRoomForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class RoomReadService extends BaseJpaRepoReadService<
         Room, RoomRepository> {
+
+    private InternalUserReadService internalUserReadService;
 
     public Page<Room> listRooms(FetchRoomForm form) {
 
@@ -29,10 +33,21 @@ public class RoomReadService extends BaseJpaRepoReadService<
             spec = spec.and(repository.branchEntityIdIs(form.getBranchEntityId()));
         }
 
+        if(StringUtils.isNotBlank(form.getSessionUserId())){
+            var user = internalUserReadService.findByEntityId(form.getSessionUserId());
+            if(user.getOrgBranch() != null){
+                spec = spec.and(repository.branchEntityIdIs(user.getOrgBranch().getEntityId()));
+            }
+        }
+
         return repository.findAll(
             spec,
             repository.defaultPageable(form)
         );
     }
 
+    @Autowired
+    public void setInternalUserReadService(InternalUserReadService service) {
+        this.internalUserReadService = service;
+    }
 }
