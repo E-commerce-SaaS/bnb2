65cf12fb73e6cfeaf0753ff55c21a7618b769593
sir-njuguna <sir.njuguna@outlook.com>
2026-08-18 10:02:57 +0300

    - Added a unique constraint to JobCardTask for job_card_id and task_id.

--- src/main/java/io/jobcardtask/entity/JobCardTask.java
+++ src/main/java/io/jobcardtask/entity/JobCardTask.java
@@ -10,15 +10,22 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(indexes = {@Index(name = "idx_job_card_entity_id_index", columnList = "jobCardEntityId")})
+@Table(
+        indexes = {@Index(name = "idx_job_card_entity_id_index", columnList = "jobCardEntityId")},
+        uniqueConstraints = {
+                @UniqueConstraint(name = "unique_constraint_job_card_task", columnNames = {"job_card_id", "task_id"})
+        }
+)
 public class JobCardTask extends BaseJpaEntity {
 
     @ManyToOne
+    @JoinColumn(name = "job_card_id")
     private JobCard jobCard;
 
     private String jobCardEntityId;
 
     @ManyToOne
+    @JoinColumn(name = "task_id")
     private Task task;
 
     @Enumerated(EnumType.STRING)
