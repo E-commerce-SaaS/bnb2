3e6f1f5081985181f1fc0ddcc67e11baf8399d6e
Davie <karanjadavid1007@gmail.com>
2026-08-20 08:16:57 +0300

    - Created endpoints for marking a job as in progress, done and inspected - Removed the ResourceNotFoundException.java IN favour of the CommonRuntimeExeption.java for use in the JobCardEditService - Created a shared preAuthorize permission annotation to be shared between related job card and job card tasks. - Allowed the user to be passing a list of task entity ids when creating a job task card. - Removed status during Job creation. - Moved all the files previously defined in JobCardtask folder into the jobcard folder. - Resolved merge conflicts by adding the appending to the messages_en.properies file.

--- src/main/java/io/jobcard/repository/JobCardTaskRepository.java
+++ src/main/java/io/jobcard/repository/JobCardTaskRepository.java
@@ -3,12 +3,11 @@ package io.jobcard.repository;
 import io.jobcard.entity.JobCardTask;
 import io.lib.repository.BaseJpaRepository;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface JobCardTaskRepository extends BaseJpaRepository<JobCardTask> , JpaSpecificationExecutor<JobCardTask> {
+public interface JobCardTaskRepository extends BaseJpaRepository<JobCardTask>{
 
     default Specification<JobCardTask> jobCardEntityIdIs(String jobCardId) {
         return (root, query, cb) -> cb.equal(root.get("jobCardEntityId"), jobCardId);
