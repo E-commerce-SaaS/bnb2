40077021c895ebc9e5fa13fe8c38b2322f9ecae4
Davie <karanjadavid1007@gmail.com>
2026-08-20 08:08:03 +0300

    - Created endpoints for marking a job as in progress, done and inspected - Removed the ResourceNotFoundException.java IN favour of the CommonRuntimeExeption.java for use in the JobCardEditService - Created a shared preAuthorize permission annotation to be shared between related job card and job card tasks. - Allowed the user to be passing a list of task entity ids when creating a job task card. - Removed status during Job creation. - Moved all the files previously defined in JobCardtask folder into the jobcard folder.

--- src/main/java/io/jobcard/repository/JobCardRepository.java
+++ src/main/java/io/jobcard/repository/JobCardRepository.java
@@ -1,15 +1,18 @@
 package io.jobcard.repository;
 
 import io.jobcard.entity.JobCard;
+import io.jobcard.entity.JobCardTask;
 import io.lib.repository.BaseJpaRepository;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 
 @Repository
-public interface JobCardRepository extends BaseJpaRepository<JobCard>, JpaSpecificationExecutor<JobCard> {
+public interface JobCardRepository extends BaseJpaRepository<JobCard>{
 
     default Specification<JobCard> staffEntityIdIs(String staffEntityId) {
         return (root, query, cb) -> cb.equal(root.get("staff").get("entityId"), staffEntityId);
--- src/main/java/io/jobcard/service/JobCardReadService.java
+++ src/main/java/io/jobcard/service/JobCardReadService.java
@@ -13,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class JobCardReadService extends BaseJpaRepoReadService<JobCard, JobCardRepository> {
