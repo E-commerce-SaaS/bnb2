29bef4742dd014147df5b49b65a0e81c3921a368
Moses Kitonga <mosesndunda939@gmail.com>
2026-08-21 13:57:09 +0300

    add customer update endpoint

--- src/main/java/io/customer/service/CustomerEditService.java
+++ src/main/java/io/customer/service/CustomerEditService.java
@@ -2,8 +2,11 @@ package io.customer.service;
 
 import io.activitylog.form.CreateActivityLogForm;
 import io.customer.entity.Customer;
+import io.customer.form.CustomerEditForm;
 import io.customer.form.CustomerRegistrationForm;
 import io.customer.repository.CustomerRepository;
+import io.lib.exception.CommonRuntimeException;
+import io.lib.exception.ExceptionType;
 import io.lib.service.BaseJpaRepoEditService;
 
 import org.springframework.stereotype.Service;
@@ -27,5 +30,34 @@ public class CustomerEditService extends BaseJpaRepoEditService<Customer, Custom
         activityLogQueuingService.enqueueActivityLog(activityLogForm);
 
         return customer;
+    }
+
+    public Customer editCustomer(String customerId, CustomerEditForm form) {
+        var customer = findByEntityId(customerId);
+
+        customer.setName(form.getName());
+        customer.setPhoneNumber(form.getPhoneNumber());
+
+        customer = save(customer ,form.getSessionUserId());
+
+        var activityLogForm = new CreateActivityLogForm();
+        activityLogForm.setOwningEntityId(customer.getEntityId());
+        activityLogForm.setAction("Customer update");
+        activityLogForm.setSessionUserId(form.getSessionUserId());
+        activityLogQueuingService.enqueueActivityLog(activityLogForm);
+
+        return customer;
+    }
+
+    private void checkNameExists(String customerId, String name){
+        var spec = repository.notDeleted()
+                .and(repository.nameLike(name))
+                .and(repository.entityIdNot(customerId));
+
+        boolean exists = repository.exists(spec);
+
+        if(exists){
+            throw new CommonRuntimeException(ExceptionType.BAD_REQUEST, "error.duplicate.name");
+        }
     }
 }
