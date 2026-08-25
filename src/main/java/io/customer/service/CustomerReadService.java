96570867a618ce102aae5a5c568465a6bc9999a8
sir-njuguna <sir.njuguna@outlook.com>
2026-08-25 17:18:16 +0300

    - Use StringUtils.isNotBlank for query validation

--- src/main/java/io/customer/service/CustomerReadService.java
+++ src/main/java/io/customer/service/CustomerReadService.java
@@ -4,6 +4,7 @@ import io.customer.entity.Customer;
 import io.customer.repository.CustomerRepository;
 import io.lib.form.BaseFetchForm;
 import io.lib.service.BaseJpaRepoReadService;
+import io.micrometer.common.util.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class CustomerReadService extends BaseJpaRepoReadService<Customer, CustomerRepository> {
     public Page<Customer> listCustomers(BaseFetchForm form) {
         var spec = repository.notDeleted();
-        if (form.getQuery() != null && !form.getQuery().isBlank()) {
+        if (StringUtils.isNotBlank(form.getQuery())) {
             spec = spec.and(repository.nameLike(form.getQuery()));
         }
 
